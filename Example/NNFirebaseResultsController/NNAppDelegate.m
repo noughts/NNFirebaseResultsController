8dfd5957674310c3926fa590a06c9ab228d9334c
Koichi Yamamoto <noughts@gmail.com>
2016-09-25 11:59:00 +0900

    a

--- Example/NNFirebaseResultsController/NNAppDelegate.m
+++ Example/NNFirebaseResultsController/NNAppDelegate.m
@@ -14,6 +14,7 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions{
 	[FIRApp configure];
+	[FIRDatabase database].persistenceEnabled = YES;
     return YES;
 }
 
